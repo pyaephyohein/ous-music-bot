cf78477ab33e20adbd5366e45beafba225796dd6
Pyae Phyo Hein <pyaephyohein.info.3326@gmail.com>
2020-11-26 22:32:11 +0630

    member status

--- bot.py
+++ bot.py
@@ -7,15 +7,17 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
 
 client = discord.Client()
-
 @client.event
 async def on_ready():
     for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+        # if guild.name == GUILD:
+        #     break
 
-    print(
-        f'{client.user} is connected to follwing server: \n'
-        f'{guild.name}(id: {guild.id})'
-        )
+        print(
+            f'{client.user} is connected to follwing server: \n'
+            f'{guild.name}(id: {guild.id}) Totle Members: {guild.member_count} \n'
+            )
+        print (client.guilds)
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f'Guild Members:\n - {members}')
 client.run(TOKEN)
